8e012af7e40cf8508986ea1f8998a20dfac23849
IsraelChidera <israelchidera54@gmail.com>
2023-01-24 16:28:49 +0100

    Refactoring

--- Program.cs
+++ Program.cs
@@ -5,20 +5,11 @@ namespace Elasto
     internal class Program
     {
         static void Main(string[] args)
-        {
-            List<Product> products = new List<Product>()
-            {
-                new Product(){ Id=1, Name="Dell Xps" ,Quantity = 30 , Price="$1500" ,Category="PCs", OrderCount=1000 },
-                new Product(){ Id=2,Name="Ergonomic Chair", Quantity=400 , Price="$200", Category="Chairs" , OrderCount=4000},
-                new Product(){ Id=3, Name="Table", Quantity=500, Price="$250", Category="Tables", OrderCount=3000},
-            };
-
+        {            
             Console.WriteLine("Hello, World!");
-            ProductCollection productCollection = new();
-            productCollection.DisplayCollections();
-            //ElasticProductCollection elasticProducts = new(products);
-            //elasticProducts.DisplayProperties();            
-            //elasticProducts.DisplayProperties("Id", "Name", "Price");
+            ProductCollection product = new();
+            product.DisplayAllProductCollection();
+            product.DisplayProductByProperties("Id");
         }
     }
 }
